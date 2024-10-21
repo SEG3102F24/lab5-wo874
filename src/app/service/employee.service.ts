8f5ffbdc7540d21eb138e258097240baec862ecc
wo874 <92176181+wo874@users.noreply.github.com>
2024-10-20 21:49:38 -0400

    Update employee.service.ts

--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -1,19 +1,53 @@
 import { Injectable } from '@angular/core';
-import {BehaviorSubject, Observable} from "rxjs";
-import {Employee} from "../model/employee";
+import { BehaviorSubject, Observable } from "rxjs";
+import { Employee } from "../model/employee";
+import { Firestore, collection, addDoc, collectionData } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
-  employees$: BehaviorSubject<readonly Employee[]> = new BehaviorSubject<readonly Employee[]>([]);
+  
+  employees$: BehaviorSubject<readonly Employee[]>=new BehaviorSubject<readonly Employee[]>([]);
 
-  get $(): Observable<readonly Employee[]> {
+  constructor(private firestore: Firestore) {
+    this.getEmployeesFromFirestore(); // Fetch employees when service is initialized
+  }
+
+  get $(): Observable < readonly Employee[]> {
     return this.employees$;
   }
-
-  addEmployee(employee: Employee) {
-    this.employees$.next([...this.employees$.getValue(), employee]);
-    return true;
+  async addEmployee(employee: Employee) {
+    try {
+      
+      const employeeCollection=collection(this.firestore, 'employees');
+      await addDoc(employeeCollection,{ ...employee }); 
+      this.employees$.next([...this.employees$.getValue(), employee]); 
+      return true;
+    } catch (error){
+      
+      console.error("Error adding employee to Firestore: ", error);
+      return false;
+      
+    }
   }
+  getEmployeesFromFirestore(){
+    const employeeCollection = collection(this.firestore, 'employees');
+    collectionData(employeeCollection, { idField: 'id' })
+      .pipe(
+        map((data) => {
+          return data.map((emp) =>({
+            ...emp,
+            dateOfBirth: emp['dateOfBirth'].toDate ? emp['dateOfBirth'].toDate() : new Date(emp['dateOfBirth'])
+          })) as Employee[];
+          
+        })
+      )
+      .subscribe((employees: Employee[]) =>{
+        this.employees$.next(employees); 
+        
+      });
+  }
+  
 }
