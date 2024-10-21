109981438070e107d16eab133d7331f933b6a361
wo874 <92176181+wo874@users.noreply.github.com>
2024-10-20 23:54:48 -0400

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+    production: false,
+    firebase: {
+      
+  apiKey: "AIzaSyCuAsvfjzDX78oZBWfyeL1_NJljYh-Ccz4",
+  authDomain: "lab5anna.firebaseapp.com",
+  projectId: "lab5anna",
+  storageBucket: "lab5anna.appspot.com",
+  messagingSenderId: "1084631067235",
+  appId: "1:1084631067235:web:38f54b8b767ee55db3e656",
+  measurementId: "G-93BT3FE2K1"
+
+    }
+
+};
